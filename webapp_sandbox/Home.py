9cc978f7196c884475013b690fb273706654ac52
Carolina Lennon <14179052+lennon-c@users.noreply.github.com>
2024-11-29 15:19:45 +0100

    Add GitHub url.

--- webapp_sandbox/Home.py
+++ webapp_sandbox/Home.py
@@ -8,7 +8,10 @@ def main():
 
     st.title("Sandbox Using Streamlit") 
     # st.sidebar.title("Pages")
-    st.markdown("Just a set of personal test exploring the Streamlit library.")
+    st.markdown("Just a set of personal test exploring the Streamlit library. ")
+
+    """
+    Code in my GitHub: [webapp_sandbox](https://github.com/lennon-c/webapp_sandbox)"""
   
 if __name__ == '__main__':
     main()
